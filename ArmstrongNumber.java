c8850ed0fc98ec9c7a646fdf418e934a5668dce8
shivaanurgpatha-lang <shivaanuragpatha@gmail.com>
2026-06-16 19:49:26 +0530

    modifies file

---
+++ ArmstrongNumber.java
@@ -0,0 +1,30 @@
+package com.java.numbers;
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number:");
+        int num = sc.nextInt();
+        int original = num;
+        @SuppressWarnings("ReassignedVariable") int count = 0;
+        @SuppressWarnings("ReassignedVariable") int sum = 0;
+        @SuppressWarnings("ReassignedVariable") int temp = num;
+
+        while (temp != 0) {
+            temp = temp / 10;
+            count++;
+        }
+        temp = num;
+        if(temp>0){
+            int digit = temp % 10;
+            sum += (int) Math.pow(digit,count);
+            temp = temp/10;
+        }
+        if (sum == original) {
+            System.out.println("Armstrong Number");
+        }
+        else {
+            System.out.println("Not Armstrong Number");
+        }
+    }
+}
